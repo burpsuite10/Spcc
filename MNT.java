5e0990fb615275ace65b6199e51edf387283916f
Ashutosh257 <36417920+Ashutosh257@users.noreply.github.com>
2018-05-01 23:04:01 +0530

    2

--- MNT.java
+++ MNT.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-class Macro
+
+class Macro 
 {
 static int mntc,mdtc;
 public static void main(String args[])
 {
 int p,j,i=0,n=5;
 String name=new String();;
-String a[]={ &quot;MACRO&quot;, &quot;ADDM&quot;, &quot;A 1,DATA&quot;, &quot;A 2 DATA&quot;, &quot;A 3 DATA&quot;, &quot;MEND&quot; ,&quot;DATA DC
-F5&quot;,&quot;END&quot;};
+String a[]={ "MACRO", "ADDM", "A 1,DATA", "A 2 DATA", "A 3 DATA", "MEND" ,"DATA DC F5","END"};
 mntc=0;
 mdtc=0;
-System.out.println(&quot;INDEX&quot;+&quot; &quot;+&quot;NAME&quot;+&quot; &quot;+&quot;MDT INDEX&quot;);
-while(a[i]!=&quot;END&quot;)
+System.out.println("INDEX"+"       "+"NAME"+"          "+"MDT INDEX");
+while(a[i]!="END")
 {
-if(a[i]==&quot;MACRO&quot;)
+if(a[i]=="MACRO")
 {
 name=a[i+1];
 break;
 }
 i++;
 }
-System.out.println(mntc+&quot; &quot;+name+&quot; &quot;+mdtc);
+System.out.println(mntc+"           "+name+"           "+mdtc);
 mntc++;
 }
 }
